ac592430cbae98884423f623e657abaec2d56ebc
masayakimura <masa_kuanoo@live.jp>
2023-03-11 19:41:01 +0900

    年の表示を削除した

--- components/convertdate.js
+++ components/convertdate.js
@@ -4,6 +4,6 @@ import 'dayjs/locale/ja'
 dayjs.locale('ja')
 
 export default function ConvertDate({ convertDate }) {
-  const publishedAt = dayjs(convertDate).format('YYYY年M月DD日(ddd)')
+  const publishedAt = dayjs(convertDate).format('M月DD日(ddd)')
   return <time dateTime={convertDate}>{publishedAt}</time>
 }
