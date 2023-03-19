49ed8af74724c6ad1d6c24f3e7510930d2468a36
masayakimura <masa_kuanoo@live.jp>
2023-03-19 19:58:21 +0900

    ライブ情報の表示をコンポーネント化

--- components/LiveInformation.tsx
+++ components/LiveInformation.tsx
@@ -1,87 +1,143 @@
-import { log } from 'console'
+import { Live } from '@/types/types'
 import Link from 'next/link'
 import { FC } from 'react'
 
-export const livesInformation = [
-  {
-    name: 'ゲキモリdeヤバイバー！Vol.30',
-    ticketUrl: 'https://t.livepocket.jp/e/glssn',
-    place: 'Club Asia',
-    placeUrl: 'https://clubasia.jp/#access',
-    liveTime: '16:25〜16:40',
-    eventTime: '17:00〜18:00',
-    priceAdd: '2k',
-    priceDoor: '2.5k',
-  },
+import ConvertDate from 'components/convertdate'
+import { getToday } from '@/pages'
 
-  {
-    name: 'FES',
-    ticketUrl: 'https://t.livepocket.jp/e/glssn',
-    place: 'deseo',
-    placeUrl: 'http://www.deseo.co.jp/map.html',
-    liveTime: '16:25〜16:40',
-    eventTime: '17:00〜18:00',
-    priceAdd: '2k',
-    priceDoor: '3k',
-  },
-  {
-    name: 'FES2',
-    ticketUrl: 'https://t.livepocket.jp/e/glssn',
-    place: 'deseo',
-    placeUrl: 'http://www.deseo.co.jp/map.html',
-    liveTime: '16:25〜16:40',
-    eventTime: '17:00〜18:00',
-    priceAdd: '2k',
-    priceDoor: '3k',
-  },
-]
+export const LiveInformation: FC<Omit<Live, 'id' | 'created_at'>> = ({
+  name,
+  ticket_url,
+  place,
+  place_url,
+  date,
+  live_start_time,
+  live_finish_time,
+  event_start_time,
+  event_finish_time,
+  price_add,
+  price_door,
+  tweet_url,
+}) => {
+  let today = getToday()
 
-type LiveInformation = {
-  name: string
-  place: string
-  liveTime: string
-  eventTime: string
-  priceAdd: string
-  priceDoor: string
-}
-
-export const LiveInformation: FC<LiveInformation> = () => {
   return (
-    <div>
-      {livesInformation.map((liveInfo) => (
-        <div key={liveInfo.name}>
-          <div className="py-4">
-            <div className="mb-1 text-center text-xl">
-              <Link href={liveInfo.ticketUrl}>{liveInfo.name}</Link>
-            </div>
-            <div className="mb-2 text-center text-xl">
-              <Link href={liveInfo.placeUrl}>{liveInfo.place}</Link>
-            </div>
-            <div className="mb-3 flex items-center justify-center">
-              <div className="mr-2 rounded-md bg-blue-500 px-1 text-lg text-white">
-                ライブ
-              </div>
-              <div className="text-xl">{liveInfo.liveTime}</div>
-            </div>
-            <div className="mb-3 flex items-center justify-center">
-              <div className="mr-2 rounded-md bg-pink-600 px-1 text-lg text-white">
-                特典会
-              </div>
-              <div className="text-xl">{liveInfo.eventTime}</div>
-            </div>
-            <div className="flex items-center justify-center">
-              <div className="mr-2 rounded-md bg-purple-800 px-3 text-lg text-white">
-                料金
-              </div>
-              <div className="text-xl">
-                ADD：{liveInfo.priceAdd}　DOOR：
-                {liveInfo.priceDoor}
-              </div>
-            </div>
+    <>
+      <div className="pb-4">
+        {today === date ? (
+          ''
+        ) : (
+          <div className="mx-auto mb-2 w-36 rounded-full bg-blue-500 py-1 text-center text-lg text-white">
+            <ConvertDate convertDate={date}></ConvertDate>
           </div>
-          <hr />
+        )}
+
+        <div className="mb-2 flex items-center justify-center text-center text-xl">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="mr-1 h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z"
+            />
+          </svg>
+
+          {tweet_url ? (
+            <Link href={tweet_url} target="_blank">
+              {name}
+            </Link>
+          ) : (
+            name
+          )}
         </div>
-      ))}
-    </div>
+        <div className="mb-3 flex items-center justify-center text-center text-xl">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="mr-1 h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+            />
+          </svg>
+          {place_url ? (
+            <Link href={place_url} target="_blank">
+              {place}
+            </Link>
+          ) : place ? (
+            place
+          ) : (
+            <p>未定</p>
+          )}
+        </div>
+        <div className="mb-3 flex items-center justify-center">
+          <div className="mr-2 rounded-md bg-blue-500 px-1 text-lg text-white">
+            ライブ
+          </div>
+          <div className="text-xl">
+            {live_start_time
+              ? `${live_start_time.slice(0, 5)}〜${live_finish_time?.slice(
+                  0,
+                  5
+                )}`
+              : '未定'}
+          </div>
+        </div>
+        <div className="mb-3 flex items-center justify-center">
+          <div className="mr-2 rounded-md bg-pink-600 px-1 text-lg text-white">
+            特典会
+          </div>
+          <div className="text-xl">
+            {event_start_time
+              ? `${event_start_time.slice(0, 5)}〜${event_finish_time?.slice(
+                  0,
+                  5
+                )}`
+              : '未定'}
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="mr-2 rounded-md bg-purple-800 px-3 text-lg text-white">
+            料金
+          </div>
+          <div className="text-xl">
+            {ticket_url ? (
+              <Link href={ticket_url} target="_blank">
+                <span className="mr-4">ADD:{price_add}K</span>
+                <span>
+                  DOOR:
+                  {price_door}K
+                </span>
+              </Link>
+            ) : (
+              <>
+                <span className="mr-4">ADD:{price_add}K</span>
+                <span>
+                  DOOR:
+                  {price_door}K
+                </span>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+      <hr className="mb-4 mt-1" />
+    </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,10 @@ import { GetServerSideProps, NextPage } from 'next'
 import { supabase } from '@/utils/supabase'
 import { Live } from '@/types/types'
 
-import ConvertDate from 'components/convertdate'
 import CardsCarousel from '@/components/carousel'
 import { Member, Pety } from '@/components/pety'
 import { Footer } from '@/components/Footer'
+import { LiveInformation } from '@/components/LiveInformation'
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const { data: lives } = await supabase
@@ -33,13 +33,17 @@ type LivesProps = {
   lives: Live[]
 }
 
-const Home: NextPage<LivesProps> = ({ lives }) => {
+export const getToday = () => {
   let date = new Date()
   let year = date.getFullYear()
   let month = ('0' + (date.getMonth() + 1)).slice(-2)
   let day = date.getDate()
   let today = `${year}-${month}-${day}`
+  return today
+}
 
+const Home: NextPage<LivesProps> = ({ lives }) => {
+  let today = getToday()
   const todayCheck = lives.map((live) => {
     if (today === live.date) {
       return true
@@ -122,115 +126,20 @@ const Home: NextPage<LivesProps> = ({ lives }) => {
       {lives.map((live) => (
         <div key={live.id}>
           {today === live.date ? (
-            <>
-              <div className="pb-4">
-                <div className="mb-2 flex items-center justify-center text-center text-xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="mr-1 h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z"
-                    />
-                  </svg>
-
-                  {live.tweet_url ? (
-                    <Link href={live.tweet_url} target="_blank">
-                      {live.name}
-                    </Link>
-                  ) : (
-                    live.name
-                  )}
-                </div>
-                <div className="mb-3 flex items-center justify-center text-center text-xl">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="mr-1 h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-                    />
-                  </svg>
-                  {live.place_url ? (
-                    <Link href={live.place_url} target="_blank">
-                      {live.place}
-                    </Link>
-                  ) : live.place ? (
-                    live.place
-                  ) : (
-                    <p>未定</p>
-                  )}
-                </div>
-                <div className="mb-3 flex items-center justify-center">
-                  <div className="mr-2 rounded-md bg-blue-500 px-1 text-lg text-white">
-                    ライブ
-                  </div>
-                  <div className="text-xl">
-                    {live.live_start_time
-                      ? `${live.live_start_time.slice(
-                          0,
-                          5
-                        )}〜${live.live_finish_time?.slice(0, 5)}`
-                      : '未定'}
-                  </div>
-                </div>
-                <div className="mb-3 flex items-center justify-center">
-                  <div className="mr-2 rounded-md bg-pink-600 px-1 text-lg text-white">
-                    特典会
-                  </div>
-                  <div className="text-xl">
-                    {live.event_start_time
-                      ? `${live.event_start_time.slice(
-                          0,
-                          5
-                        )}〜${live.event_finish_time?.slice(0, 5)}`
-                      : '未定'}
-                  </div>
-                </div>
-                <div className="flex items-center justify-center">
-                  <div className="mr-2 rounded-md bg-purple-800 px-3 text-lg text-white">
-                    料金
-                  </div>
-                  <div className="text-xl">
-                    {live.ticket_url ? (
-                      <Link href={live.ticket_url} target="_blank">
-                        <span className="mr-4">ADD:{live.price_add}K</span>
-                        <span>
-                          DOOR:
-                          {live.price_door}K
-                        </span>
-                      </Link>
-                    ) : (
-                      <>
-                        <span className="mr-4">ADD:{live.price_add}K</span>
-                        <span>
-                          DOOR:
-                          {live.price_door}K
-                        </span>
-                      </>
-                    )}
-                  </div>
-                </div>
-              </div>
-              <hr className="mb-4 mt-1" />
-            </>
+            <LiveInformation
+              name={live.name}
+              ticket_url={live.ticket_url}
+              place={live.place}
+              place_url={live.place_url}
+              date={live.date}
+              live_start_time={live.live_start_time}
+              live_finish_time={live.live_finish_time}
+              event_start_time={live.event_start_time}
+              event_finish_time={live.event_finish_time}
+              price_add={live.price_add}
+              price_door={live.price_door}
+              tweet_url={live.tweet_url}
+            />
           ) : (
             <></>
           )}
@@ -248,124 +157,20 @@ const Home: NextPage<LivesProps> = ({ lives }) => {
           {lives.map((live) => (
             <div key={live.id}>
               {!(today === live.date) && today < live.date ? (
-                <>
-                  <div className="pb-4">
-                    <div className="mx-auto mb-2 w-36 rounded-full bg-blue-500 py-1 text-center text-lg text-white">
-                      <ConvertDate convertDate={live.date}></ConvertDate>
-                    </div>
-                    <div className="mb-2 flex items-center justify-center text-center text-xl">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="mr-1 h-6 w-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z"
-                        />
-                      </svg>
-
-                      {live.tweet_url ? (
-                        <Link href={live.tweet_url} target="_blank">
-                          {live.name}
-                        </Link>
-                      ) : live.name ? (
-                        live.name
-                      ) : (
-                        <p>未定</p>
-                      )}
-                    </div>
-                    <div className="mb-3 flex items-center justify-center text-center text-xl">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="mr-1 h-6 w-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-                        />
-                      </svg>
-                      {live.place_url ? (
-                        <Link href={live.place_url} target="_blank">
-                          {live.place}
-                        </Link>
-                      ) : live.place ? (
-                        live.place
-                      ) : (
-                        <p>未定</p>
-                      )}
-                    </div>
-                    <div className="mb-3 flex items-center justify-center">
-                      <div className="mr-2 rounded-md bg-blue-500 px-1 text-lg text-white">
-                        ライブ
-                      </div>
-                      <div className="text-xl">
-                        {live.live_start_time
-                          ? `${live.live_start_time.slice(
-                              0,
-                              5
-                            )}〜${live.live_finish_time?.slice(0, 5)}`
-                          : '未定'}{' '}
-                      </div>
-                    </div>
-                    <div className="mb-3 flex items-center justify-center">
-                      <div className="mr-2 rounded-md bg-pink-600 px-1 text-lg text-white">
-                        特典会
-                      </div>
-                      <div className="text-xl">
-                        {live.event_start_time
-                          ? `${live.event_start_time.slice(
-                              0,
-                              5
-                            )}〜${live.event_finish_time?.slice(0, 5)}`
-                          : '未定'}{' '}
-                      </div>
-                    </div>
-                    <div className="flex items-center justify-center">
-                      <div className="mr-2 rounded-md bg-purple-800 px-3 text-lg text-white">
-                        料金
-                      </div>
-                      <div className="text-xl">
-                        {live.ticket_url ? (
-                          <Link href={live.ticket_url} target="_blank">
-                            <span className="mr-4">ADD:{live.price_add}K</span>
-                            <span>
-                              DOOR:
-                              {live.price_door}K
-                            </span>
-                          </Link>
-                        ) : live.price_add ? (
-                          <>
-                            <span className="mr-4">ADD:{live.price_add}K</span>
-                            <span>
-                              DOOR:
-                              {live.price_door}K
-                            </span>
-                          </>
-                        ) : (
-                          <>
-                            <span>未定</span>
-                          </>
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                  <hr className="mb-5 mt-1" />
-                </>
+                <LiveInformation
+                  name={live.name}
+                  ticket_url={live.ticket_url}
+                  place={live.place}
+                  place_url={live.place_url}
+                  date={live.date}
+                  live_start_time={live.live_start_time}
+                  live_finish_time={live.live_finish_time}
+                  event_start_time={live.event_start_time}
+                  event_finish_time={live.event_finish_time}
+                  price_add={live.price_add}
+                  price_door={live.price_door}
+                  tweet_url={live.tweet_url}
+                />
               ) : (
                 <></>
               )}
