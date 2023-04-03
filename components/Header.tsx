7fded090e83e210bf08824ad21fdb3ccbccacf11
masayakimura <masa_kuanoo@live.jp>
2023-04-04 02:05:17 +0900

    元の方法に戻す

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
-import GoogleAnalytics from './GoogleAnalytics'
+import Script from 'next/script'
+
+import { GA_ID, existsGaId } from '../src/lib/gtag'
 
 type Props = {
   title: string
@@ -22,8 +24,24 @@ export const Header = (props: Props) => {
       <meta property="og:site_name" content={title} />
       <meta property="og:type" content={type} />
       <meta property="og:image" content={imageUrl} />
-
-      <GoogleAnalytics />
+      {/* Google Analytics */}
+      {existsGaId && (
+        <>
+          <Script
+            defer
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga" defer strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());    
+              gtag('config', '${GA_ID}');
+          `}
+          </Script>
+        </>
+      )}
     </Head>
   )
 }
