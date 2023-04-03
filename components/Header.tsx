7e97246e2c9af05cdeae76c9e632f61185af2b10
masayakimura <masa_kuanoo@live.jp>
2023-04-04 02:10:19 +0900

    アナリティクス削除

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Script from 'next/script'
-
-import { GA_ID, existsGaId } from '../src/lib/gtag'
 
 type Props = {
   title: string
@@ -24,24 +21,6 @@ export const Header = (props: Props) => {
       <meta property="og:site_name" content={title} />
       <meta property="og:type" content={type} />
       <meta property="og:image" content={imageUrl} />
-      {/* Google Analytics */}
-      {existsGaId && (
-        <>
-          <Script
-            defer
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script id="ga" defer strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());    
-              gtag('config', '${GA_ID}');
-          `}
-          </Script>
-        </>
-      )}
     </Head>
   )
 }
